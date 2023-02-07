08ae8fd0a643cd3a386d2c0d0c45a2d5d6ea7cae
avifeld99 <avifeld99@gmail.com>
2023-02-07 11:52:36 +0100

    added

---
+++ src/Oefeningen/TimeBomb.java
@@ -0,0 +1,23 @@
+package Oefeningen;
+
+public class TimeBomb implements Runnable {
+
+    private int seconds;
+
+    public TimeBomb(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void activate() {
+
+    }
+
+    public void disarm() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
